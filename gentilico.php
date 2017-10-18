9c1f8d7985a6daf55ccbc34bb64edfda0fcdde2d
ivyson10 <ivysonlucas10@gmail.com>
2017-10-18 11:53:37 -0300

    exercicio 3

---
+++ gentilico.php
@@ -0,0 +1,85 @@
+<?php
+
+$s = $_GET["sigla"];
+
+
+
+if($s=="AC")
+    echo "O seu gentilico e: acriano";
+
+elseif($s=="AL")
+    echo "O seu gentilico e: alagoano";
+
+
+elseif($s=="AP")
+    echo "O seu gentilico e: amapaense";
+
+
+elseif ($s=="AM")
+    echo "O seu gentilico e: amazonense";
+
+elseif($s=="BA")
+    echo "O seu gentilico e: baiano";
+
+elseif($s=="CE")
+    echo "O seu gentilico e: cearense";
+
+elseif ($s=="DF")
+    echo "O seu gentilico e: brasiliense";
+elseif($s=="ES")
+    echo "O seu gentilico e: capixaba ou espirito-santense";
+
+elseif($s=="GO")
+    echo "O seu gentilico e: goiano";
+
+elseif ($s=="MA")
+    echo "O seu gentilico e: maranhense";
+elseif($s=="MT")
+    echo "O seu gentilico e: mato-grossense";
+
+elseif($s=="MS")
+    echo "O seu gentilico e: sul-mato-grossense";
+
+elseif ($s=="MG")
+    echo "O seu gentilico e: mineiro";
+
+elseif ($s=="PA")
+    echo "O seu gentilico e: paraense";
+elseif($s=="PB")
+    echo "O seu gentilico e: paraibano";
+
+elseif($s=="PR")
+    echo "O seu gentilico e: paraense";
+
+elseif ($s=="PE")
+    echo "O seu gentilico e: pernambuco";
+elseif($s=="PI")
+    echo "O seu gentilico e: piauiense";
+
+elseif($s=="RJ")
+    echo "O seu gentilico e: fluminense";
+
+elseif ($s=="RN")
+    echo "O seu gentilico e: pontiguar";
+
+
+elseif ($s=="RS")
+    echo "O seu gentilico e: gaucho";
+elseif($s=="RO")
+    echo "O seu gentilico e: rondoniano";
+
+elseif($s=="RR")
+    echo "O seu gentilico e: roraimense";
+
+elseif ($s=="SC")
+    echo "O seu gentilico e: catarinense";
+
+
+elseif ($s=="SP")
+    echo "O seu gentilico e: paulista";
+elseif($s=="SE")
+    echo "O seu gentilico e: sergipano";
+
+elseif($s=="TO")
+    echo "O seu gentilico e: tocantinense";
+?>
